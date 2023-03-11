e3c41663ce5bf0282cab6409a04d31b1fc126d10
sukhvindersingh <sukhvindersingh2513@gmail.com>
2023-03-11 13:10:47 +0530

    UI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  baseUrl:'http://localhost:3000'
-  // baseUrl:'https://nodejsstartup.onrender.com'
+  // baseUrl:'http://localhost:3000'
+  baseUrl:'https://nodejsstartup.onrender.com'
   // baseUrl:'http://167.71.228.227:3000'
  // http://sukhvindersingh.lovestoblog.com/'
 
